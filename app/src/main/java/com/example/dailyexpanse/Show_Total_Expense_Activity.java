a15ec36394ce6739a08d6e2f6c6719012c397966
Muzahidul Islam <53990772+muzahidul123@users.noreply.github.com>
2019-10-19 23:34:13 +0600

    Update Show_Total_Expense_Activity.java

--- app/src/main/java/com/example/dailyexpanse/Show_Total_Expense_Activity.java
+++ app/src/main/java/com/example/dailyexpanse/Show_Total_Expense_Activity.java
@@ -5,10 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Show_Total_Expense_Activity extends AppCompatActivity {
+    
+    private Spinner spinner;
+    private  TextView fromdateTV, todateTV;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show__total__expense_);
+        
+        init();
+        
+    }
+    
+     private void init() {
+
+        spinner = findViewById(R.id.spinner);
+        fromdateTV = findViewById(R.id.fromdateTV);
+        todateTV = findViewById(R.id.todateTV);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
     }
 }
