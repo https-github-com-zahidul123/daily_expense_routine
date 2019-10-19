d2add58d8604ba2771f4b1de9038885f3a22338e
Muzahidul Islam <muzahidul123@gmail.com>
2019-10-19 23:21:58 +0600

    launcher activity design

---
+++ app/src/main/java/com/example/dailyexpanse/Show_Total_Expense_Activity.java
@@ -0,0 +1,14 @@
+package com.example.dailyexpanse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Show_Total_Expense_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show__total__expense_);
+    }
+}
